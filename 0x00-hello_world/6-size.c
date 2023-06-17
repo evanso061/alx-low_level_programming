fc735b66d2db0f8c5f1976eeca7ef274381be42f
evanso061 <evansotieno061@gmail.com>
2023-06-17 06:18:11 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - My main entry point
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+printf("Size of a char; %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int; %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long init; %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int; %lu bytes(s)\n" (unsigned long)sizeof(d));
+printf("Size of a float; %lu bytes(s)\n" (unsigned long)sizeof(e);
+return (0);
+}
