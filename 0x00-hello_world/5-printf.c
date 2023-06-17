8a7b803ddabf897d13cde442b17321091473f2da
evanso061 <evansotieno061@gmail.com>
2023-06-17 05:48:16 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
  * main - My main entry point
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
 
