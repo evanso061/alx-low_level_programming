3aafc6b7739f442582489147cc5d4f8348ac9a37
evanso061 <evansotieno061@gmail.com>
2023-06-21 08:19:08 -0700

    my if,else and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - My positive or negative zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for  (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
