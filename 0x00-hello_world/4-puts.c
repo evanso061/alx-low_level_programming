ec3021381eed7d5850c0f0dd55e23caab97c23de
evanso061 <evansotieno061@gmail.com>
2023-06-23 04:57:28 -0700

    4-puts_redo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *  main - My main entry point
+ *
  *  Return 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
 
