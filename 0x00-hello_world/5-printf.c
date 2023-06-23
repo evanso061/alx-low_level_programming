41abbf7438c17b06ab70e18b98e7309727f4baf2
evanso061 <evansotieno061@gmail.com>
2023-06-23 05:19:27 -0700

    fives printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - My main entry point
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
