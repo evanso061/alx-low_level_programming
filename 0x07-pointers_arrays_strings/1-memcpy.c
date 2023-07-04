eef1b3e6517fdbb130088fd93b4a97a194ea9ba3
evanso061 <evansotieno061@gmail.com>
2023-07-04 06:38:05 -0700

    copy the memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - coping a  memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: filled byte.
+ * Return: pointer dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
+}
