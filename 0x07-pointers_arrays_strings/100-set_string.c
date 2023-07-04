8eeaf82e8f8cb9dad23f18ad4f3e4433f0a3ebf1
evanso061 <evansotieno061@gmail.com>
2023-07-04 08:46:49 -0700

    sets pointer value to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets pointer value to char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
